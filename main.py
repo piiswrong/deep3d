ad08efa7a4364d0ae3562ec704dbf79c12b5c53b
jason toy <jasontoy@gmail.com>
2016-05-03 22:14:50 +0000

    json support

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ ap.add_argument("-i", "--image", required=True, help="path to base image")
 ap.add_argument("-j", "--json",default=False,  help="upload images")
 ap.add_argument("-o", "--output_folder", default="./", help="output folder")
 args = ap.parse_args()
+param_path = '/tmp/deep3d-0050.params'
+if not os.path.exists(param_path):
+      urllib.urlretrieve('http://homes.cs.washington.edu/~jxie/download/deep3d-0050.params', param_path)
+      model = mx.model.FeedForward.load('/tmp/deep3d', 50, mx.gpu(0))
 
-if not os.path.exists('deep3d-0050.params'):
-      urllib.urlretrieve('http://homes.cs.washington.edu/~jxie/download/deep3d-0050.params', 'deep3d-0050.params')
-      model = mx.model.FeedForward.load('deep3d', 50, mx.gpu(0))
-
-model = mx.model.FeedForward.load('deep3d', 50, mx.gpu(0))
+model = mx.model.FeedForward.load('/tmp/deep3d', 50, mx.gpu(0))
 
 shape = (384, 160)
 img = cv2.imread(args.image)
@@ -50,11 +50,12 @@ if args.json != False:
     right.save(right_path,"JPEG") 
     left.save(left_path,"JPEG") 
 
-    right_output,re = subprocess.check_output(["curl", "--upload-file", right_path,("https://transfer.sh/"+right_name)]);
-    left_output,le = subprocess.check_output(["curl", "--upload-file", left_path,("https://transfer.sh/"+left_name)]);
-    data = {right:right_output,left:left_output}
-    put_file =  args.output_folder + "/"+ str(int(time.time())) + ".json"
-    with open(put_file, 'w') as outfile:
+    right_output = subprocess.check_output(["curl","-s", "--upload-file", right_path,("https://transfer.sh/"+right_name)]).strip()
+    left_output = subprocess.check_output(["curl", "-s","--upload-file", left_path,("https://transfer.sh/"+left_name)]).strip()
+    data = {'right':right_output,'left':left_output}
+    output_file = args.output_folder + "/"+ str(int(time.time())) + ".json"
+    print(data)
+    with open(output_file, 'w') as outfile:
         json.dump(data, outfile)
 else:
     put_file = args.output_folder + "/"+ str(int(time.time())) + ".gif"
